c76576aef03ce763b23e7030b9fa7e8ca49ed11b
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-03-24 16:20:31 +0300

    Add files via upload

--- util/ResultsData.js
+++ util/ResultsData.js
@@ -1,1 +1,176 @@
+export const RESULTS = [
+    { 
+        semester:1,
+        year:1,
+        id: 1,
+        course_code: "1",
+        course_title: "Essential hardware and Software Concepts",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:1,
+        year:1,
+        id: 2,
+        course_code: "2",
+        course_title: "Fundamentals of Computing",
+        Grade: 4,
+        credit_unit: 4
+    },
+    { 
+        semester:1,
+        year:1,
+        id: 3,
+        course_code: "3",
+        course_title: "Structured Programming",
+        Grade: 4.5,
+        credit_unit: 4
+    },
+    { 
+        semester:1,
+        year:1,
+        id: 4,
+        course_code: "4",
+        course_title: "Discrete Mathematics",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:1,
+        year:1,
+        id: 5,
+        course_code: "5",
+        course_title: "Writing and Study Skills",
+        Grade: 5,
+        credit_unit: 3
+    },
+    { 
+        semester:1,
+        year:1,
+        id: 6,
+        course_code: "6",
+        course_title: "Understanding the Old Testament",
+        Grade: 5,
+        credit_unit: 3
+    },
+    { 
+        semester:2,
+        year:1,
+        id: 7,
+        course_code: "7",
+        course_title: "Data Structures and Algorithms",
+        Grade: 5,
+        credit_unit: 4
+    },
 
+    { 
+        semester:2,
+        year:1,
+        id: 8,
+        course_code: "8",
+        course_title: "Computer Organisation and Architecture",
+        Grade: 4,
+        credit_unit: 3
+    },
+    { 
+        semester:2,
+        year:1,
+        id: 2,
+        course_code: "9",
+        course_title: "Probability and Statistics",
+        Grade: 4,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:1,
+        id: 3,
+        course_code: "10",
+        course_title: "Database design and Algorithms",
+        Grade: 4.5,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:1,
+        id: 4,
+        course_code: "11",
+        course_title: "Local Area Computer Networking",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:1,
+        id: 5,
+        course_code: "12",
+        course_title: "Understanding New Testament",
+        Grade: 5,
+        credit_unit: 3
+    },
+    { 
+        semester:2,
+        year:1,
+        id: 6,
+        course_code: "13",
+        course_title: "Health and Wholeness",
+        Grade: 5,
+        credit_unit: 3
+    },
+    { 
+        semester:2,
+        year:2,
+        id: 7,
+        course_code: "14",
+        course_title: "Object Oriented Programming",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:2,
+        id: 7,
+        course_code: "15",
+        course_title: "Operating Systems",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:2,
+        id: 7,
+        course_code: "16",
+        course_title: "Design and analysis of Algorithms",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:2,
+        id: 7,
+        course_code: "18",
+        course_title: "Calculus",
+        Grade: 5,
+        credit_unit: 3
+    },
+    { 
+        semester:2,
+        year:2,
+        id: 7,
+        course_code: "180",
+        course_title: "Data Science",
+        Grade: 5,
+        credit_unit: 4
+    },
+    { 
+        semester:2,
+        year:2,
+        id: 7,
+        course_code: "160",
+        course_title: "Understanding Ehtics from a Christian Perspective",
+        Grade: 5,
+        credit_unit: 4
+    }
+];
+
+// export const COURSES
